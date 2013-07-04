8b7ea45dbc58e3b25356d57e700072aa244496a5
Yevgeniy Medvedev <emedvedev@thumbtack.net>
2013-07-04 11:39:25 +0700

    bullet and wall performance

--- bullet-and-wall.py
+++ bullet-and-wall.py
@@ -3,9 +3,11 @@ Input: Four lists with coordinates--each one is a list of x and y coordinates--W
 Output: Whether the bullet hits the wall or not, aka True or False.
 """
 from math import fabs
+import datetime
 
 
-def checkio(data):
+# 4 seconds 651000 microseconds
+def checkio1(data):
 
     def in_beam(end, sign, x):
         if sign >= 0.0 and x <= end:
@@ -53,6 +55,52 @@ def checkio(data):
 
         return is_beam_ok and is_wall_ok
 
+# 1 seconds 616000 microseconds
+def checkio2(data):
+
+    u1, u2 = data[0]
+    v1, v2 = data[1]
+    a1, a2 = data[2]
+    b1, b2 = data[3]
+
+    c11, c12, c21, c22 = v1 - u1, a1 - b1, v2 - u2, a2 - b2
+    d1, d2 = a1 - u1, a2 - u2
+
+    D = 1. * (c11 * c22 - c12 * c21)
+    Dx = d1 * c22 - d2 * c12
+    Dy = c11 * d2 - c21 * d1
+
+    if D != 0:
+        x, y = Dx / D, Dy / D
+        return 0 <= x <= 1 and y >= 0
+
+    return Dx == 0 and 1. * d1 / c12 > 0
+
+
+# 2 seconds 416000 microseconds
+def checkio(data):
+
+    def cross_product(v1,v2):
+        # return applicate component only
+        return v1[0]*v2[1] - v1[1]*v2[0]
+
+    def dot_product(v1,v2):
+        return v1[0]*v2[0] + v1[1]*v2[1]
+
+    def between(v, v1, v2):
+        prd = cross_product(v1,v2)
+        if abs(prd) < 0.000001:
+            return dot_product(v, v1) > 0 or dot_product(v, v2) > 0
+        else:
+            return cross_product(v1,v)*prd >= 0 and cross_product(v,v2)*prd >= 0
+
+    xw1, yw1 = data[0]
+    xw2, yw2 = data[1]
+    xa, ya = data[2]
+    xb, yb = data[3]
+
+    return between((xb-xa,yb-ya), (xw1-xa,yw1-ya), (xw2-xa,yw2-ya))
+
 #Some hints
 #You can search intersection point for lines
 #Or look to rays geometry
@@ -60,10 +108,17 @@ def checkio(data):
 
 #These "asserts" using only for self-checking and not necessary for auto-testing
 if __name__ == '__main__':
-    assert checkio([[0, 1], [1, 2], [2, 0], [0, 2]]) == True, "0st example"
-    assert checkio([[0, 0], [0, 2], [5, 1], [3, 1]]) == True, "1st example"
-    assert checkio([[0, 0], [0, 2], [3, 1], [5, 1]]) == False, "2nd example"
-    assert checkio([[0, 0], [2, 2], [6, 0], [3, 1]]) == True, "3rd example"
-    assert checkio([[6, 0], [5, 5], [4, 0], [5, 6]]) == False, "4th example"
-    assert checkio([[0, 0], [1, 1], [3, 3], [2, 2]]) == True, "5th example"
-    assert checkio([[0, 0], [1, 1], [3, 2], [2, 1]]) == False, "6th example"
+    start_time = datetime.datetime.now()
+    for i in range(100000):
+        assert checkio([[0, 1], [1, 2], [2, 0], [0, 2]]) == True, "0st example"
+        assert checkio([[0, 0], [0, 2], [5, 1], [3, 1]]) == True, "1st example"
+        assert checkio([[0, 0], [0, 2], [3, 1], [5, 1]]) == False, "2nd example"
+        assert checkio([[0, 0], [2, 2], [6, 0], [3, 1]]) == True, "3rd example"
+        assert checkio([[6, 0], [5, 5], [4, 0], [5, 6]]) == False, "4th example"
+        assert checkio([[0, 0], [1, 1], [3, 3], [2, 2]]) == True, "5th example"
+        assert checkio([[0, 0], [1, 1], [3, 2], [2, 1]]) == False, "6th example"
+
+    end_time = datetime.datetime.now()
+    c = end_time - start_time
+
+    print '%d seconds %d microseconds' % (c.seconds, c.microseconds)
