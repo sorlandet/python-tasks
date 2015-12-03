526e3432bb9ca43c2708be068c359e8129288123
Evgeniy Medvedev <yevgeniy.medvedev@gmail.com>
2015-12-03 18:33:54 +0600

    checking regular expression + some fixes

--- personal/check.py
+++ personal/check.py
@@ -4,58 +4,49 @@ import sys
 import re
 
 text = """
-<td class="greyText" width="307" valign="top" height="113">
-<span class="redText">
-<br>
-Price:
-<strong>
-<br>
-PropertyID:
-<strong>207</strong>
-<br>
-Type:
-<strong>Apartment</strong>
-<br>
-Bedrooms:
-<strong>1</strong>
-<br>
-Bathrooms:
-<strong>1</strong>
-<br>
-WC:
-<strong>0</strong>
-<br>
-Pool:
-<strong>Communal Pool </strong>
-<br>
-Building Area:
-<strong>63.30sqm</strong>
-<br>
-District:
-<strong> Pafos </strong>
-<br>
-Suburb:
-<strong> Geroskipou </strong>
-<br>
-Title Deeds:
-<strong>No</strong>
-<br>
-</td>
+<td width="307" valign="top" height="113" class="greyText">
+                <span class="redText"><strong>
+                Resale                </strong></span><br>
+
+
+
+
+
+                 Price: <strong><span onclick="showAlbum('detailedInfo207', 'ping.php?propertyID=207', '400');" style="cursor:pointer" class="tealText">
+                 75,000 </span></strong><br>
+
+                  PropertyID: <strong>207</strong><br>
+                  Type: <strong>Apartment</strong><br>
+
+                  Bedrooms: <strong>1</strong><br>
+                  Bathrooms: <strong>1</strong><br>
+                  WC: <strong>0</strong><br>
+                  Pool:
+                  <strong>Communal Pool                  </strong><br>
+                  Building Area: <strong>63.30sqm</strong><br>
+                  District:<strong> Pafos </strong><br>
+                  Suburb:<strong> Geroskipou </strong><br>
+                  Title Deeds: <strong>No</strong><br>                   </td>
 """
 
 
 def main(argv):
     global text
     ret = {}
-    for item in text.split('<br>'):
-        # print item
-        p = re.compile('(?P<key>.*):<strong>(?P<value>.*)</strong>', re.DOTALL)
-        m = p.match(item.replace("\n", ""))
+    for line in text.split('<br>'):
+        item = line.strip()
+        item = re.sub(' +', ' ', item)
+        p = re.compile('(?P<key>.*|.*(?:\s.*)):\s?<strong>(?P<value>.*)</strong>', re.DOTALL)
+        item = item.replace("\n", "")
+        m = p.match(item)
         if m:
             key, value = m.groups()
-            ret[key] = value
+            ret[key.strip()] = value.strip()
 
-    print ret
+    import pprint
+    pp = pprint.PrettyPrinter(depth=6)
+    pp.pprint(ret)
+
 
 if __name__ == '__main__':
     start_time = datetime.datetime.now()
