ea0371fb584f52cd2127c41b6aa27b56312aae2f
Evgeniy Medvedev <emedvedev@thumbtack.net>
2015-12-17 16:27:01 +0600

    adding few elementary checkio missions

---
+++ checkio/elementary/count-inversions.py
@@ -0,0 +1,22 @@
+def count_inversion(sequence):
+    """
+        Count inversions in a sequence of numbers
+    """
+    changes = 0
+    seq = list(sequence)
+    for i in range(1, len(seq)):
+        j = i
+        while j > 0 and seq[j - 1] > seq[j]:
+            seq[j - 1], seq[j] = seq[j], seq[j - 1]
+            j -= 1
+            changes += 1
+
+    return changes
+
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert count_inversion((1, 2, 5, 3, 4, 7, 6)) == 3, "Example"
+    assert count_inversion((0, 1, 2, 3)) == 0, "Sorted"
+    assert count_inversion((99, -99)) == 1, "Two numbers"
+    assert count_inversion((5, 3, 2, 1, 0)) == 10, "Reversed"
