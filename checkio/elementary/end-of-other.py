dbdea43ae4d9e2fc6b698135c9bcb70e154ce73e
Evgeniy Medvedev <emedvedev@thumbtack.net>
2015-12-17 16:59:24 +0600

    adding few elementary checkio missions

---
+++ checkio/elementary/end-of-other.py
@@ -0,0 +1,17 @@
+def checkio(words_set):
+    for word in words_set:
+        other_words = words_set.copy()
+        other_words.remove(word)
+        for el in other_words:
+            if el.endswith(word):
+                return True
+
+    return False
+
+#These "asserts" using only for self-checking and not necessary for auto-testing
+if __name__ == '__main__':
+    assert checkio({"hello", "lo", "he"}) == True, "helLO"
+    assert checkio({"hello", "la", "hellow", "cow"}) == False, "hellow la cow"
+    assert checkio({"walk", "duckwalk"}) == True, "duck to walk"
+    assert checkio({"one"}) == False, "Only One"
+    assert checkio({"helicopter", "li", "he"}) == False, "Only end"
